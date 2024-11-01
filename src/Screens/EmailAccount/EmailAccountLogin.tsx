d02f6ac981991fcdfd1348ca5934123c693c6661
Yogesh Gurjar <yogesh.gurjar_14208@email.ide.builder.ai>
2024-11-01 23:00:48 +0530

    [01/09/2024] updated final ui for email account login

--- src/Screens/EmailAccount/EmailAccountLogin.tsx
+++ src/Screens/EmailAccount/EmailAccountLogin.tsx
@@ -1,134 +1,128 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React, { Component } from 'react'
+import { Image, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class EmailAccountLogin extends Component {
     render() {
 
         return (
             <View style={styles.container}>
-            {/* Back Arrow */}
-            <TouchableOpacity style={styles.backButton}>
-              {/* <Icon name="arrow-back" size={24} color="white" /> */}
-            </TouchableOpacity>
-      
-            {/* Title */}
-            <Text style={styles.title}>Log in to LoveDKY</Text>
-      
-            {/* Email Input */}
-            <Text style={styles.label}>Email</Text>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} placeholder="hello@company.com" placeholderTextColor="#555" />
+
+                {/* Title */}
+                <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 60 }}>
+                    <Image
+                        style={{ height: 90, width: 250, marginBottom: 40 }}
+                        source={{
+                            uri: 'https://lovedky.com/cdn/shop/files/dky_logo_with_wings_white-01.png',
+                        }} />
+                </View>
+
+
+                {/* Header */}
+                <View style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 40 }}>
+                    <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>Welcome to LoveDKY</Text>
+                    <Text style={{ color: 'white', fontSize: 15, marginTop: 5, fontStyle: 'italic' }}>Elevate Your Wardrobe, Embrace Your Style</Text>
+                </View>
+
+                {/* Email Input */}
+                <View style={styles.inputContainer}>
+                    <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#555" />
+                </View>
+
+                {/* Password Input */}
+                <View style={styles.inputContainer}>
+                    <TextInput style={styles.input} placeholder="Password" placeholderTextColor="#555" secureTextEntry />
+                    <Icon name="visibility-off" size={20} color="#555" style={styles.icon} />
+                </View>
+
+                {/* Forgot Password */}
+                <TouchableOpacity>
+                    <Text style={styles.forgotPassword}>Forgot Password?</Text>
+                </TouchableOpacity>
+
+                {/* Login Button */}
+                <TouchableOpacity style={styles.loginButton}>
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableOpacity>
+
+                {/* Footer Text */}
+                <Text style={styles.footerText}>
+                    Don't Have an Account? <Text style={{ fontWeight: 'bold', color: 'white' }}> Sign Up </Text>
+                </Text>
             </View>
-      
-            {/* Password Input */}
-            <Text style={styles.label}>Password</Text>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} placeholder="Your password" placeholderTextColor="#555" secureTextEntry />
-              {/* <Icon name="visibility-off" size={20} color="#555" style={styles.icon} /> */}
-            </View>
-      
-            {/* Forgot Password */}
-            <TouchableOpacity>
-              <Text style={styles.forgotPassword}>Forgot Password?</Text>
-            </TouchableOpacity>
-      
-            {/* Login Button */}
-            <TouchableOpacity style={styles.loginButton}>
-              <Text style={styles.loginText}>Login</Text>
-            </TouchableOpacity>
-      
-            {/* OR Divider */}
-            <View style={styles.divider}>
-              <View style={styles.line} />
-              <Text style={styles.orText}>OR</Text>
-              <View style={styles.line} />
-            </View>
-      
-            {/* Social Buttons */}
-            <TouchableOpacity style={styles.socialButton}>
-              {/* <FontAwesome name="apple" size={20} color="white" /> */}
-              <Text style={styles.socialText}>Continue with Apple</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.socialButton}>
-              {/* <FontAwesome name="google" size={20} color="white" /> */}
-              <Text style={styles.socialText}>Continue with Google</Text>
-            </TouchableOpacity>
-      
-            {/* Footer Text */}
-            <Text style={styles.footerText}>
-              Scale uses cookies for analytics personalized content and ads. By using Scale's services you agree to this use of cookies. <Text style={styles.learnMore}>Learn more</Text>
-            </Text>
-          </View>
         );
     }
 }
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#000',
         padding: 20,
-      },
-      backButton: {
-        marginBottom: 20,
-      },
-      title: {
-        alignSelf:'center',
+    },
+    backButton: {
+
+    },
+    tinyLogo: {
+        height: '10%', width: '10%', backgroundColor: 'yellow'
+    },
+    title: {
+        // alignSelf: 'center',
         fontSize: 28,
         color: 'white',
         fontWeight: 'bold',
-        marginBottom: 20,
-      },
-      label: {
+        // marginBottom: 20,
+    },
+    label: {
         color: 'white',
         fontSize: 14,
         marginVertical: 10,
-      },
-      inputContainer: {
+    },
+    inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: '#1e1e1e',
         borderRadius: 8,
         paddingHorizontal: 10,
         marginBottom: 15,
-      },
-      input: {
+    },
+    input: {
         flex: 1,
         color: 'white',
         paddingVertical: 10,
-      },
-      icon: {
+    },
+    icon: {
         padding: 5,
-      },
-      forgotPassword: {
+    },
+    forgotPassword: {
         color: 'green',
         marginVertical: 10,
-      },
-      loginButton: {
+    },
+    loginButton: {
         backgroundColor: 'white',
         paddingVertical: 12,
         borderRadius: 8,
         alignItems: 'center',
         marginVertical: 20,
-      },
-      loginText: {
+    },
+    loginText: {
         color: 'black',
         fontWeight: 'bold',
-      },
-      divider: {
+    },
+    divider: {
         flexDirection: 'row',
         alignItems: 'center',
         marginVertical: 20,
-      },
-      line: {
+    },
+    line: {
         flex: 1,
         height: 1,
         backgroundColor: '#555',
-      },
-      orText: {
+    },
+    orText: {
         color: '#555',
         marginHorizontal: 10,
-      },
-      socialButton: {
+    },
+    socialButton: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: '#1e1e1e',
@@ -136,19 +130,19 @@ const styles=StyleSheet.create({
         borderRadius: 8,
         marginBottom: 10,
         paddingHorizontal: 20,
-      },
-      socialText: {
+    },
+    socialText: {
         color: 'white',
         marginLeft: 10,
-      },
-      footerText: {
-        color: '#555',
-        fontSize: 12,
+    },
+    footerText: {
+        color: 'grey',
+        fontSize: 15,
         textAlign: 'center',
-        marginTop: 20,
-      },
-      learnMore: {
+        marginTop: 10,
+    },
+    learnMore: {
         color: 'white',
         textDecorationLine: 'underline',
-      },
+    },
 })
